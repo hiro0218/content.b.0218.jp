2b60645d43d124f71ad6472db194ce3b7276b659
hiro0218 <hiro0218@gmail.com>
2018-07-28 23:58:53 +0900

    add: service workerの登録処理を追加

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,22 @@ import App from './App.vue';
 import pagination from 'vuejs-uib-pagination';
 import { escapeBrackets, dateToISOString, formatBaseLink, formatDate } from '@scripts/utils';
 
+const config = require('../config.json');
+
+// Service Worker
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+      .register(config.publicPath + '/dist/sw.js', { scope: '/' })
+      .then(registration => {
+        registration.update();
+      })
+      .catch(err => {
+        console.log('[Service Worker] failed: ', err);
+      });
+  });
+}
+
 Vue.use(pagination);
 
 // Vue global mixin
