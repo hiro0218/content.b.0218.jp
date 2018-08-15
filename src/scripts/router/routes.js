6d625dd932f17fca7d6518d127de0af9c84413a0
hiro0218 <hiro0218@gmail.com>
2018-08-16 00:01:36 +0900

    modify: loop method

--- src/scripts/router/routes.js
+++ src/scripts/router/routes.js
@@ -5,11 +5,9 @@ const pageArchive = () => import(/* webpackChunkName: "other" */ '@/pages/archiv
 
 let routes = [];
 
-for (let type in WP.routes) {
-  let wp_route = WP.routes[type];
-
-  for (let key in wp_route) {
-    let route = wp_route[key];
+for(let type of Object.keys(WP.routes)) {
+  for(let i = 0; i < WP.routes[type].length; i++) {
+    let route = WP.routes[type][i];
     let temp = {};
 
     temp = {
