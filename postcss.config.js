7b477bc92432ba7dff242988e7c4d1459db26b88
hiro0218 <hiro0218@gmail.com>
2018-09-19 23:16:22 +0900

    fix: 不要な定義を削除

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,3 @@
-const cssnanoConfig = {
-  autoprefixer: false,
-  colormin: true,
-  convertValues: true,
-  cssDeclarationSorter: true,
-  discardComments: { removeAll: true },
-  discardDuplicates: true,
-  discardEmpty: true,
-  discardOverridden: true,
-  mergeLonghand: true,
-  mergeRules: true,
-  minifyFontValues: true,
-  minifySelectors: true,
-  uniqueSelectors: true,
-  svgo: true,
-};
-
 module.exports = ctx => ({
   parser: require('postcss-safe-parser'),
   plugins: [
@@ -24,16 +7,16 @@ module.exports = ctx => ({
       grid: true,
       cascade: false,
     }),
-    require('cssnano')({
-      preset: ['default', cssnanoConfig],
-    }),
-    require('postcss-nesting'),
     require('postcss-zindex'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       stage: 3,
     }),
     require('css-mqpacker'),
-    ctx.env === 'production' && require('csswring'),
+    require('cssnano')({
+      preset: ['default', {
+        autoprefixer: false,
+      }],
+    }),
   ],
 });
